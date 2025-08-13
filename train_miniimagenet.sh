725d268448cc98e6c4bb5dce49c68d2b4d149e11
ParkGyumin <gyumin1129@kookmin.ac.kr>
2025-08-13 20:25:09 +0900

    mini imagenet 정상

--- train_miniimagenet.sh
+++ train_miniimagenet.sh
@@ -2,5 +2,5 @@ GPUS=1
 work_dir=work_dirs/mamba_fscil/miniimagenet_resnet12_mambafscil
 #bash tools/dist_train.sh configs/mini_imagenet/resnet12_etf_bs512_500e_miniimagenet_mambafscil.py $GPUS --work-dir ${work_dir} --seed 0 --deterministic
 # latest.pth를 자동으로 찾아서 base 세션에서 이어서 학습
-RESUME_LATEST=true bash tools/dist_train.sh configs/mini_imagenet/resnet12_etf_bs512_500e_miniimagenet_mambafscil.py $GPUS --work-dir ${work_dir} --seed 0 --deterministic
-bash tools/run_fscil.sh configs/mini_imagenet/resnet12_etf_bs512_500e_miniimagenet_eval_mambafscil.py ${work_dir} ${work_dir}/best.pth $GPUS --seed 0 --deterministic
+RESUME_LATEST=true bash tools/dist_train.sh configs/mini_imagenet/vmamba_base_etf_bs512_500e_miniimagenet_mambafscil.py $GPUS --work-dir ${work_dir} --seed 0 --deterministic
+bash tools/run_fscil.sh configs/mini_imagenet/vmamba_base_etf_bs512_500e_miniimagenet_eval_mambafscil.py ${work_dir} ${work_dir}/best.pth $GPUS --seed 0 --deterministic
