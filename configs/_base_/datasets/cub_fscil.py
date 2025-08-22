0bb1a46afa5edd91b00c0a335f2c7403021d07bb
ParkGyumin <gyumin1129@kookmin.ac.kr>
2025-08-22 21:03:35 +0900

    데이터셋 최적화(CUB)

--- configs/_base_/datasets/cub_fscil.py
+++ configs/_base_/datasets/cub_fscil.py
@@ -1,3 +1,4 @@
+# dataset settings
 img_size = 224
 _img_resize_size = 256
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
@@ -6,16 +7,73 @@ img_norm_cfg = dict(mean=[123.675, 116.28, 103.53],
 meta_keys = ('filename', 'ori_filename', 'ori_shape', 'img_shape', 'flip',
              'flip_direction', 'img_norm_cfg', 'cls_id', 'img_id')
 
-train_pipeline = [
+# 원본 이미지용 파이프라인
+original_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='RandomResizedCrop', size=img_size),
-    dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
-    dict(type='ColorJitter', brightness=0.4, contrast=0.4, saturation=0.4),
+    dict(type='Resize', size=(_img_resize_size, _img_resize_size)),
+    dict(type='CenterCrop', crop_size=img_size),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
     dict(type='ToTensor', keys=['gt_label']),
     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
 ]
+
+# # 증강 이미지용 파이프라인
+# augmented_pipeline = [
+#     dict(type='LoadAugmentedImage',
+#          aug_dir='data/CUB_200_2011/augmented_images'),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='ImageToTensor', keys=['img']),
+#     dict(type='ToTensor', keys=['gt_label']),
+#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
+# ]
+
+# # 증강 이미지용 파이프라인
+# augmented_pipeline1 = [
+#     dict(type='LoadAugmentedImage',
+#          aug_dir='data/CUB_200_2011/augmented_images_1'),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='ImageToTensor', keys=['img']),
+#     dict(type='ToTensor', keys=['gt_label']),
+#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
+# ]
+# # 증강 이미지용 파이프라인
+# augmented_pipeline2= [
+#     dict(type='LoadAugmentedImage',
+#          aug_dir='data/CUB_200_2011/augmented_images_2'),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='ImageToTensor', keys=['img']),
+#     dict(type='ToTensor', keys=['gt_label']),
+#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
+# ]
+# # 증강 이미지용 파이프라인
+# augmented_pipeline3 = [
+#     dict(type='LoadAugmentedImage',
+#          aug_dir='data/CUB_200_2011/augmented_images_3'),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='ImageToTensor', keys=['img']),
+#     dict(type='ToTensor', keys=['gt_label']),
+#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
+# ]
+# # 증강 이미지용 파이프라인
+# augmented_pipeline4 = [
+#     dict(type='LoadAugmentedImage',
+#          aug_dir='data/CUB_200_2011/augmented_images_4'),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='ImageToTensor', keys=['img']),
+#     dict(type='ToTensor', keys=['gt_label']),
+#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
+# ]
+# # 회전된 이미지용 파이프라인 (별도 폴더)
+# rotated_pipeline = [
+#     dict(type='LoadRotatedImage', rot_dir='data/CUB_200_2011/rotated_images'),
+#     dict(type='Resize', size=(_img_resize_size, _img_resize_size)),
+#     dict(type='CenterCrop', crop_size=img_size),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='ImageToTensor', keys=['img']),
+#     dict(type='ToTensor', keys=['gt_label']),
+#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
+# ]
 
 test_pipeline = [
     dict(type='LoadImageFromFile'),
@@ -26,31 +84,32 @@ test_pipeline = [
     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
 ]
 
-data = dict(samples_per_gpu=64,
-            workers_per_gpu=8,
-            train_dataloader=dict(persistent_workers=True, ),
-            val_dataloader=dict(persistent_workers=True, ),
-            test_dataloader=dict(persistent_workers=True, ),
-            train=dict(type='RepeatDataset',
-                       times=4,
-                       dataset=dict(
-                           type='CUBFSCILDataset',
-                           data_prefix='./data/CUB_200_2011',
-                           pipeline=train_pipeline,
-                           num_cls=100,
-                           subset='train',
-                       )),
-            val=dict(
-                type='CUBFSCILDataset',
-                data_prefix='./data/CUB_200_2011',
-                pipeline=test_pipeline,
-                num_cls=100,
-                subset='test',
-            ),
-            test=dict(
-                type='CUBFSCILDataset',
-                data_prefix='./data/CUB_200_2011',
-                pipeline=test_pipeline,
-                num_cls=200,
-                subset='test',
-            ))
+data = dict(
+    samples_per_gpu=32,
+    workers_per_gpu=1,
+    train_dataloader=dict(persistent_workers=True),
+    val_dataloader=dict(persistent_workers=True),
+    test_dataloader=dict(persistent_workers=True),
+    train=dict(type='RepeatDataset',
+                times=1,
+                dataset=dict(
+                    type='CUBFSCILDataset',
+                    data_prefix='./data/CUB_200_2011',
+                    pipeline=original_pipeline,
+                    num_cls=100,
+                    subset='train',
+                )),
+    val=dict(
+        type='CUBFSCILDataset',
+        data_prefix='./data/CUB_200_2011',
+        pipeline=test_pipeline,
+        num_cls=100,
+        subset='test',
+    ),
+    test=dict(
+        type='CUBFSCILDataset',
+        data_prefix='./data/CUB_200_2011',
+        pipeline=test_pipeline,
+        num_cls=200,
+        subset='test',
+    ))
