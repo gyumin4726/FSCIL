497843cfecad71af5582abc1409d378057b62439
gyumin4726 <gyumin1129@kookmin.ac.kr>
2025-09-24 17:42:51 +0900

    MoEMLP NECK 레지스터리에 추가

--- mmfscil/models/__init__.py
+++ mmfscil/models/__init__.py
@@ -2,5 +2,6 @@ from .classifier import ImageClassifierCIL
 from .ETFHead import DRLoss, ETFHead
 from .mamba_ffn_neck import MambaNeck
 from .moe_fscil_neck import MoEFSCILNeck
+from .moe_fscil_neck_MLP import MoEFSCILNeckMLP
 from .resnet18 import ResNet18
 from .vmamba_backbone import VMambaBackbone
