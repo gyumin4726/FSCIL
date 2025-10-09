9e7ef66c0a8770f99382243c89dfdc5f72036848
gyumin4726 <gyumin1129@kookmin.ac.kr>
2025-10-10 08:44:04 +0900

    Direction-Wise Data Loader 삭제

--- configs/_base_/datasets/cub_fscil.py
+++ configs/_base_/datasets/cub_fscil.py
@@ -18,63 +18,6 @@ original_pipeline = [
     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
 ]
 
-# # 증강 이미지용 파이프라인
-# augmented_pipeline = [
-#     dict(type='LoadAugmentedImage',
-#          aug_dir='data/CUB_200_2011/augmented_images'),
-#     dict(type='Normalize', **img_norm_cfg),
-#     dict(type='ImageToTensor', keys=['img']),
-#     dict(type='ToTensor', keys=['gt_label']),
-#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
-# ]
-
-# # 증강 이미지용 파이프라인
-# augmented_pipeline1 = [
-#     dict(type='LoadAugmentedImage',
-#          aug_dir='data/CUB_200_2011/augmented_images_1'),
-#     dict(type='Normalize', **img_norm_cfg),
-#     dict(type='ImageToTensor', keys=['img']),
-#     dict(type='ToTensor', keys=['gt_label']),
-#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
-# ]
-# # 증강 이미지용 파이프라인
-# augmented_pipeline2= [
-#     dict(type='LoadAugmentedImage',
-#          aug_dir='data/CUB_200_2011/augmented_images_2'),
-#     dict(type='Normalize', **img_norm_cfg),
-#     dict(type='ImageToTensor', keys=['img']),
-#     dict(type='ToTensor', keys=['gt_label']),
-#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
-# ]
-# # 증강 이미지용 파이프라인
-# augmented_pipeline3 = [
-#     dict(type='LoadAugmentedImage',
-#          aug_dir='data/CUB_200_2011/augmented_images_3'),
-#     dict(type='Normalize', **img_norm_cfg),
-#     dict(type='ImageToTensor', keys=['img']),
-#     dict(type='ToTensor', keys=['gt_label']),
-#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
-# ]
-# # 증강 이미지용 파이프라인
-# augmented_pipeline4 = [
-#     dict(type='LoadAugmentedImage',
-#          aug_dir='data/CUB_200_2011/augmented_images_4'),
-#     dict(type='Normalize', **img_norm_cfg),
-#     dict(type='ImageToTensor', keys=['img']),
-#     dict(type='ToTensor', keys=['gt_label']),
-#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
-# ]
-# # 회전된 이미지용 파이프라인 (별도 폴더)
-# rotated_pipeline = [
-#     dict(type='LoadRotatedImage', rot_dir='data/CUB_200_2011/rotated_images'),
-#     dict(type='Resize', size=(_img_resize_size, _img_resize_size)),
-#     dict(type='CenterCrop', crop_size=img_size),
-#     dict(type='Normalize', **img_norm_cfg),
-#     dict(type='ImageToTensor', keys=['img']),
-#     dict(type='ToTensor', keys=['gt_label']),
-#     dict(type='Collect', keys=['img', 'gt_label'], meta_keys=meta_keys)
-# ]
-
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='Resize', size=(_img_resize_size, -1)),
